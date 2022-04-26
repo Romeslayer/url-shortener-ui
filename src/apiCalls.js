51393762a6098c20c3a3d42059957f4e16d9da94
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-26 09:34:43 -0600

    Create POST request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,18 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrl = (newURL) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: { "Content-Type": "application/json"},
+    body: JSON.stringify(newURL)
+  })
+    .then(res => {
+      if(!res.ok) {
+        throw new Error('URL not ADDED')
+      } else {
+        return res.json();
+      }
+    })
+}

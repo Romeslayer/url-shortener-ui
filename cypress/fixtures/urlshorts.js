c21445ca20c513e1e34a8db874d0eb30818dca6f
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-26 10:49:59 -0600

    Create intercepts and first test

---
+++ cypress/fixtures/urlshorts.js
@@ -0,0 +1,22 @@
+export const exampleURLS = {
+  "urls" : [
+    {
+      "id": 1,
+      "long_url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      "short_url": "http://localhost:3001/example1",
+      "title": "Rocky balboa"
+    },
+    {
+      "id": 2,
+      "long_url": "https://www.youtube.com/watch?v=prZBPTfcrbA",
+      "short_url": "http://localhost:3001/example2",
+      "title": "Ricky bobby"
+    }
+  ]
+}
+
+export const examplePost = {
+  "id": 3, "long_url": "https://images.unsplash.com/photo...",
+   "short_url": "http://localhost:3001/useshorturl/2",
+    "title": "Awesome photo"
+}
---
+++ cypress/integration/dasboard_spec.js
@@ -0,0 +1,20 @@
+import { exampleURLS, examplePost } from '../fixtures/urlshorts.js';
+console.log(exampleURLS, examplePost);
+describe('URL Shortener', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/urls', exampleURLS)
+      .as('get-urls')
+
+    cy.intercept('POST', 'http://localhost:3001/api/v1/urls', examplePost)
+      .as('post-url')
+
+      cy.visit('http://localhost:3000/')
+
+  })
+
+
+  it('When a user visits the page, they can view the page title and the existing shortened URLs', () => {
+    cy.get('header')
+      .contains('URL Shortener')
+  })
+})
